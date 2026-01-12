04003645bde1fe6836fdc7f766040d52258e330b
nit-coder <99726116+nit-coder@users.noreply.github.com>
2026-01-12 10:05:20 -0600

    fix lint issue

--- src/types/rc-image-preview.d.ts
+++ src/types/rc-image-preview.d.ts
@@ -9,3 +9,17 @@ declare module 'rc-image/es/PreviewGroup' {
     onChange?: (current: number, prevCurrent: number) => void;
   }
 }
+
+declare module '@lobehub/ui/es/Image/type' {
+  interface PreviewGroupPreviewOptions {
+    onChange?: (current: number, prevCurrent: number) => void;
+  }
+}
+
+declare module '@lobehub/ui/lib/Image/type' {
+  interface PreviewGroupPreviewOptions {
+    onChange?: (current: number, prevCurrent: number) => void;
+  }
+}
+
+export {};
