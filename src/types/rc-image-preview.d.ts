e69c24c2bf454aa630831530815980f619822f13
nit-coder <99726116+nit-coder@users.noreply.github.com>
2026-01-19 09:52:49 -0600

    fix build issue

--- src/types/rc-image-preview.d.ts
+++ src/types/rc-image-preview.d.ts
@@ -1,24 +1,28 @@
 declare module 'rc-image/lib/PreviewGroup' {
   interface PreviewGroupPreview {
     onChange?: (current: number, prevCurrent: number) => void;
+    onVisibleChange?: (value: boolean, prevValue: boolean, current: number) => void;
   }
 }
 
 declare module 'rc-image/es/PreviewGroup' {
   interface PreviewGroupPreview {
     onChange?: (current: number, prevCurrent: number) => void;
+    onVisibleChange?: (value: boolean, prevValue: boolean, current: number) => void;
   }
 }
 
 declare module '@lobehub/ui/es/Image/type' {
   interface PreviewGroupPreviewOptions {
     onChange?: (current: number, prevCurrent: number) => void;
+    onVisibleChange?: (value: boolean, prevValue: boolean, current: number) => void;
   }
 }
 
 declare module '@lobehub/ui/lib/Image/type' {
   interface PreviewGroupPreviewOptions {
     onChange?: (current: number, prevCurrent: number) => void;
+    onVisibleChange?: (value: boolean, prevValue: boolean, current: number) => void;
   }
 }
 
